18ac69ae1817e12cbe5320f61f1a2b62bd7b2e85
DURGESH JAISWAL <98084091+durgeshjaiswal77@users.noreply.github.com>
2022-06-16 12:32:17 +0530

    Add files via upload

---
+++ SpringBootDemo/src/main/java/com/durgesh/SpringBootDemo/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.durgesh.SpringBootDemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	@RequestMapping("/")  
+	public String hello()   
+	{  
+	return "Hello Durgesh";  
+	}  
+}
